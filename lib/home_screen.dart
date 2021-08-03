8e37ffb31626d34b2a1f2e3f51c5fc2b8ece6e6c
fighteros <fighteros121212@gmail.com>
2021-08-03 23:50:16 +0200

    decoration()

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -34,28 +34,35 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                Image(
-                  image: NetworkImage(
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSefdQfM6ZNihgghV7IGLORhw5aG5XzQ2kDxQ&usqp=CAU"),
-                  height: 200.0,
-                  width: 250.0,
-                  fit: BoxFit.cover,
-                ),
-                Container(
-                  color: Colors.black.withOpacity(.6),
-                  width: 250.0,
-                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                  child: Text(
-                    "Flower",
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),
-                    textAlign: TextAlign.center,
+            padding: const EdgeInsets.all(50.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Image(
+                    image: NetworkImage(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSefdQfM6ZNihgghV7IGLORhw5aG5XzQ2kDxQ&usqp=CAU"),
+                    height: 200.0,
+                    width: 250.0,
+                    fit: BoxFit.cover,
                   ),
-                ),
-              ],
+                  Container(
+                    color: Colors.black.withOpacity(.6),
+                    width: 250.0,
+                    padding:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                    child: Text(
+                      "Flower",
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
