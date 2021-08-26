1950f45f0fb535f466be9a2da0d9fafc9b863afb
fighteros <fighteros121212@gmail.com>
2021-08-26 06:15:18 +0200

    async && await and error handling

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/modules/archived_tasks/archived_tasks.dart';
@@ -30,10 +32,14 @@ class _HomeLayoutState extends State<HomeLayout> {
         title: Text(titles[currentIndex]),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          var name = await getName();
-          print(name);
-        },
+        onPressed: () => getName().then((value){
+          //  the return type of an error handler is not acceptable as the value of the future.
+          // after lambda expression is executed it gone so we wouldn't know where the error came from
+          // because also the return type is dynamic so we can't get to an datatype interface so it may be null since it's dynamic
+          if(value is FutureOr) {
+            throw("error");
+          }
+        }).catchError((error) =>  print(error.toString()) ) ,
         child: Icon(Icons.add),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -63,5 +69,5 @@ class _HomeLayoutState extends State<HomeLayout> {
     );
   }
 
-  Future<String> getName() async => 'Ahmed Ali';
+  Future<String> getName() async =>'Ahmed Ali';
 }
